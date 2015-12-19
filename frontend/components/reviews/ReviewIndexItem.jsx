656fc27c615fc6ce3d04dd2bb62c1f579fb3344c
Byron Mo <byronmo@gmail.com>
2015-12-18 17:27:27 -0800

    Start review edit form

--- frontend/components/reviews/ReviewIndexItem.jsx
+++ frontend/components/reviews/ReviewIndexItem.jsx
@@ -1,9 +1,16 @@
 var React = require('react'),
-    ApiUtil = require('../../util/apiutil');
+    ApiUtil = require('../../util/apiutil'),
+    LinkedStateMixin = require('react-addons-linked-state-mixin');
 
 var ReviewIndexItem = React.createClass({
+  mixins: [LinkedStateMixin],
+
+  getInitialState: function() {
+    return { toggle: false, rating: "", body: "" }
+  },
+
   handleEdit: function() {
-    console.log("in handleedit")
+    this.setState({ toggle: !this.state.toggle })
   },
 
   handleDelete: function(id) {
@@ -13,11 +20,53 @@ var ReviewIndexItem = React.createClass({
     ApiUtil.destroyReview(this.props.review.id);
   },
 
+  handleSubmit: function(e) {
+    e.preventDefault();
+    debugger
+    if (this.state.rating === "" && this.state.body !== "") {
+      ApiUtil.updateReview({
+        body: this.state.body
+      })
+    } else if (this.state.body === "" && this.state.rating !== "") {
+      
+    } else if (this.state.body !== "" && this.state.rating !== "") {
+
+    }
+  },
+
+  updateRating: function(e) {
+    this.setState({rating: parseInt(e.target.value)})
+  },
+
   render: function() {
+    var editForm;
+
+    if (this.state.toggle) {
+      editForm = (
+        <form className="review-edit-form" onSubmit={this.handleSubmit}>
+          <div className="star-rating">
+            <input type="radio" name="rating" value="1" onChange={this.updateRating}></input><i></i>
+            <input type="radio" name="rating" value="2" onChange={this.updateRating}></input><i></i>
+            <input type="radio" name="rating" value="3" onChange={this.updateRating}></input><i></i>
+            <input type="radio" name="rating" value="4" onChange={this.updateRating}></input><i></i>
+            <input type="radio" name="rating" value="5" onChange={this.updateRating}></input><i></i>
+          </div>
+          <br></br>
+          <textarea className="review-text" valueLink={this.linkState("body")}></textarea>
+          <br></br>
+          <input type="submit" value="Create Review" className="review-btn"></input>
+        </form>
+      )
+    } else {
+      editForm = <div></div>;
+    }
+
+
     return(
       <div>
         <input type="button" value="Edit" className="edit-btn" onClick={this.handleEdit}></input>
         <input type="button" value="Delete" className="delete-btn" onClick={this.handleDelete}></input>
+        {editForm}
       </div>
     )
   }
