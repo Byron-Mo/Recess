3759178db2ec5a9eb752f072637949d6482f8af8
Byron Mo <byronmo@gmail.com>
2015-12-30 18:10:33 -0800

    Seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -42,21 +42,9 @@
 # Puerto Rico
 # Jerusalem
 
-# 3: {
-#   name: "Cancun, Mexico",
-#   region: "North America",
-#   activity: "Beachfront",
-#   body: "Cancún, a Mexican city on the Yucatán Peninsula bordering the Caribbean Sea, is known for its beaches, numerous resorts and energetic nightlife.",
-#   image:
-# },
 
-# 5: {
-#   name: "Miami, Florida",
-#   region: "North America",
-#   activity: "Beachfront",
-#   body: "Miami, at Florida's southeastern tip, is a vibrant city whose Cuban influence is reflected in the cafes and cigar shops that line Calle Ocho in Little Havana.",
-#   image:
-# },
+
+
 
 ActiveRecord::Base.transaction do
   locations = {
@@ -194,6 +182,227 @@ ActiveRecord::Base.transaction do
       image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2482/v1450823433/316f8e75_pihmib.jpg",
       lat: 49.283884,
       lng: -123.117392
+    },
+    "16" => {
+      name: "Las Vegas, Nevada, United States",
+      region: "North America",
+      activity: "Adventure",
+      body: "Las Vegas, in Nevada’s Mojave Desert, is a resort town famed for its buzzing energy, 24-hour casinos and endless entertainment options.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2420/v1451525219/photo-1450869088552-c7e6c1c74c16_hesqgz.jpg",
+      lat: 36.169276,
+      lng: -115.148915
+    },
+    "17" => {
+      name: "Washington D.C., United States",
+      region: "North America",
+      activity: "Culture/History",
+      body: "Washington, DC, the U.S. capital, is a compact city on the Potomac River, bordering the states of Maryland and Virginia.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2462/v1451525012/unsplash_5243a2eb2bc02_1_lifr2z.jpg",
+      lat: 38.905740,
+      lng: -77.036541
+    },
+    "18" => {
+      name: "Los Angeles, California, United States",
+      region: "North America",
+      activity: "Adventure",
+      body: "Los Angeles is a sprawling Southern California city famed as the center of the nation’s film and television industry.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2533/v1451525363/photo-1444723121867-7a241cacace9_awd3jl.jpg",
+      lat: 34.050907,
+      lng: -118.246969
+    },
+    "19" => {
+      name: "Bora Bora, French Polynesia",
+      region: "Oceania",
+      activity: "Beachfront",
+      body: "Bora Bora is a small South Pacific island northwest of Tahiti in French Polynesia. Surrounded by sand-fringed motus (islets) and a turquoise lagoon protected by a coral reef, it’s known for its scuba diving.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451527769/5033-bora-bora-1920x1080-beach-wallpaper_zgoy9f.jpg",
+      lat: -16.496310,
+      lng: -151.740914
+    },
+    "20" => {
+      name: "Sao Paulo, Brazil",
+      region: "South America",
+      activity: "Adventure",
+      body: "Vibrant São Paulo is among the world's most populous cities. Brazil's financial center, it has abundant cultural institutions and a rich architectural tradition.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451525847/saopaulo_ts7opq.jpg",
+      lat: -23.551299,
+      lng: -46.637903
+    },
+    "21" => {
+      name: "Shanghai, China",
+      region: "Asia",
+      activity: "Culture/History",
+      body: "Enormous Shanghai, on China’s central coast, is the country's biggest city and a global financial hub. Its heart is the Bund, a famed waterfront promenade lined with colonial-era buildings.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451526237/Shanghai-HD-Wallpaper_pmsovg.jpg",
+      lat: 31.242832,
+      lng: 121.476720
+    },
+    "22" => {
+      name: "Beijing, China",
+      region: "Asia",
+      activity: "Culture/History",
+      body: "Beijing, China’s massive capital, has history stretching back 3 millennia. Yet it’s known as much for its modern architecture as its ancient sites such as the grand Forbidden City complex, the imperial palace during the Ming and Qing dynasties.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451526393/forbidden_city_beijing-wide_bvwpcv.jpg",
+      lat: 39.909056,
+      lng: 116.411163
+    },
+    "23" => {
+      name: "Tokyo, Japan",
+      region: "Asia",
+      activity: "Culture/History",
+      body: "Tokyo, Japan’s bustling capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers and anime shops to cherry trees and temples.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451526632/World___Japan_Street_Akihabara_in_Tokyo_058699__zfuumi.jpg",
+      lat: 35.722333,
+      lng: 139.732353
+    },
+    "24" => {
+      name: "Seoul, South Korea",
+      region: "Asia",
+      activity: "Culture/History",
+      body: "Seoul, the capital of South Korea, is a sprawling metropolis where hyper-modern skyscrapers, high-tech subways and pop culture meet Buddhist temples, palaces and street markets.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/v1451526782/pROGFgC_wds1rb.jpg",
+      lat: 37.557941,
+      lng: 126.992289
+    },
+    "25" => {
+      name: "Kuala Lumpur, Malaysia",
+      region: "Asia",
+      activity: "Adventure",
+      body: "Kuala Lumpur, the capital of Malaysia, is called KL by locals. Its modern skyline is dominated by the 451m-tall Petronas Twin Towers, a pair of glass-and-steel-clad skyscrapers with Islamic motifs.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2788/v1451526956/Kuala_Lumpur_Ultra_HD_w8kzuv.jpg",
+      lat: 3.139686,
+      lng: 101.688570
+    },
+    "26" => {
+      name: "Singapore, Singapore",
+      region: "Asia",
+      activity: "Adventure",
+      body: "Singapore, an island city-state off southern Malaysia, is a global financial centre with a tropical climate and multicultural population.",
+      image: "http://res.cloudinary.com/dptviwjop/image/upload/c_scale,w_2538/v1451527079/photo-1440780336166-b19530cff5ec_tcotbl.jpg",
+      lat: 1.355228,
+      lng: 103.861430
+    },
+    "27" => {
+      name: "Saigon, Vietname",
+      region: "Asia",
+      activity: "Culture/History",
+      body: "Saigon is a city in southern Vietnam famous for the pivotal role it played in the Vietnam War. It's also known for its French colonial architecture, including Notre-Dame Basilica, made entirely of materials imported from France, and the neoclassical Saigon Central Post Office.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "28" => {
+      name: "Saint Petersburg, Russia",
+      region: "Europe",
+      activity: "Culture/History",
+      body: "St. Petersburg is a Russian port city on the Baltic Sea. Founded in 1703 by Peter the Great, it was the imperial capital for 2 centuries, and it remains the country’s cultural center, with venues like the hypermodern Mariinsky Theatre hosting opera and ballet.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "29" => {
+      name: "Moscow, Russia",
+      region: "Europe",
+      activity: "Culture/History",
+      body: "Moscow, on the Moskva River in western Russia, is the nation’s cosmopolitan capital. In its historic core is the Kremlin, a fortified complex that’s home to the president and tsarist treasures in the Armoury.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "30" => {
+      name: "Berlin, Germany",
+      region: "Europe",
+      activity: "Culture/History",
+      body: "Berlin, Germany’s capital and cultural center, dates to the 13th century. Divided during the Cold War, today it's known for its art scene, nightlife and modern architecture, such as Mies van der Rohe’s landmark Neue Nationalgalerie.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "31" => {
+      name: "Frankfurt, Germany",
+      region: "Europe",
+      activity: "Culture/History",
+      body: "Frankfurt, a central German city on the Main River, is a major financial hub.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "32" => {
+      name: "Male, Maldives",
+      region: "Asia",
+      activity: "Beachfront",
+      body: "The Maldives is a tropical nation in the Indian Ocean composed of 26 coral atolls, which are made up of hundreds of islands. It’s known for its beaches, blue lagoons and extensive reefs.",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "33" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "34" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "35" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "36" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "37" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "38" => {
+      name: "",
+      region: "",
+      activity: "",
+      body: "",
+      image: "",
+      lat: ,
+      lng:
+    },
+    "39": {
+      name: "Miami, Florida",
+      region: "North America",
+      activity: "Beachfront",
+      body: "Miami, at Florida's southeastern tip, is a vibrant city whose Cuban influence is reflected in the cafes and cigar shops that line Calle Ocho in Little Havana.",
+      image:
+    },
+    "40": {
+      name: "Cancun, Mexico",
+      region: "North America",
+      activity: "Beachfront",
+      body: "Cancún, a Mexican city on the Yucatán Peninsula bordering the Caribbean Sea, is known for its beaches, numerous resorts and energetic nightlife.",
+      image:
     }
   }
 
